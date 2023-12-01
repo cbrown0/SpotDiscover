b4185be1238b4cd5b50b6079f1ba5de07609f680
Carter Brown <carter.brown@trojans.dsu.edu>
2023-12-01 11:08:29 -0600

    Added First API Calls

--- DiscoverSpot/DiscoverSpot/Program.cs
+++ DiscoverSpot/DiscoverSpot/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SpotifyAPI.Web;
 
 namespace DiscoverSpot
 {
@@ -12,8 +13,13 @@ namespace DiscoverSpot
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
+            var spotify = new SpotifyClient("YourAccessToken");
+
+            var track = await spotify.Tracks.Get("1s6ux0lNiTziSrd7iUAADH");
+            Console.WriteLine(track.Name);
+
             Application.EnableVisualStyles(); //gahghfaskhgkhgah
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
