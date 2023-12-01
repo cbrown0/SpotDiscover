58a8f152542d5072658ae00a9a04f28d0461ec76
gschof <gschof02@gmail.com>
2023-12-01 10:22:17 -0600

    added comment in Form1_Load()

--- DiscoverSpot/DiscoverSpot/Form1.cs
+++ DiscoverSpot/DiscoverSpot/Form1.cs
@@ -22,7 +22,7 @@ namespace DiscoverSpot
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //grace was here
         }
 
         private void label1_Click(object sender, EventArgs e)
