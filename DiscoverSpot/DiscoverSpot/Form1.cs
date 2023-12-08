6615197527e82473f5d7036fa86ea1012a534a19
isabellebakker <Isabelle.Bakker@trojans.dsu.edu>
2023-12-08 15:22:31 -0600

    authentication button functional

--- DiscoverSpot/DiscoverSpot/Form1.cs
+++ DiscoverSpot/DiscoverSpot/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,7 @@ namespace DiscoverSpot
 {
     public partial class Form1 : Form
     {
+        private static EmbedIOAuthServer _server;
         public Form1()
         {
             //hi
@@ -27,12 +30,44 @@ namespace DiscoverSpot
 
         private void label1_Click(object sender, EventArgs e)
         {
-            // This is a label
+            
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
+            // This is a label
+            // Make sure "http://localhost:5543/callback" is in your spotify application as redirect uri!
+            _server = new EmbedIOAuthServer(new Uri("http://localhost:5543/callback"), 5543);
+            await _server.Start();
 
+            _server.AuthorizationCodeReceived += OnAuthorizationCodeReceived;
+            _server.ErrorReceived += OnErrorReceived;
+
+            var request = new LoginRequest(_server.BaseUri, "07d2f610371745aba056026392538495", LoginRequest.ResponseType.Code)
+            {
+                Scope = new List<string> { Scopes.UserReadEmail }
+            };
+            BrowserUtil.Open(request.ToUri());
+        }
+        private static async Task OnAuthorizationCodeReceived(object sender, AuthorizationCodeResponse response)
+        {
+            await _server.Stop();
+
+            var config = SpotifyClientConfig.CreateDefault();
+            var tokenResponse = await new OAuthClient(config).RequestToken(
+              new AuthorizationCodeTokenRequest(
+                "ClientId", "ClientSecret", response.Code, new Uri("http://localhost:5543/callback")
+              )
+            );
+
+            var spotify = new SpotifyClient(tokenResponse.AccessToken);
+            // do calls with Spotify and save token?
+        }
+
+        private static async Task OnErrorReceived(object sender, string error, string state)
+        {
+            Console.WriteLine($"Aborting authorization, error received: {error}");
+            await _server.Stop();
         }
     }
 }
--- DiscoverSpot/DiscoverSpot/Program.cs
+++ DiscoverSpot/DiscoverSpot/Program.cs
@@ -13,44 +13,14 @@ namespace DiscoverSpot
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        // [STAThread] not sure why this needs to be commented out.
-        private static EmbedIOAuthServer _server;
-
-        public static async Task Main()
+        
+        
+        [STAThread]
+        static void Main()
         {
-            // Make sure "http://localhost:5543/callback" is in your spotify application as redirect uri!
-            _server = new EmbedIOAuthServer(new Uri("http://localhost:5543/callback"), 5543);
-            await _server.Start();
-
-            _server.AuthorizationCodeReceived += OnAuthorizationCodeReceived;
-            _server.ErrorReceived += OnErrorReceived;
-
-            var request = new LoginRequest(_server.BaseUri, "07d2f610371745aba056026392538495", LoginRequest.ResponseType.Code)
-            {
-                Scope = new List<string> { Scopes.UserReadEmail }
-            };
-            BrowserUtil.Open(request.ToUri());
-        }
-
-        private static async Task OnAuthorizationCodeReceived(object sender, AuthorizationCodeResponse response)
-        {
-            await _server.Stop();
-
-            var config = SpotifyClientConfig.CreateDefault();
-            var tokenResponse = await new OAuthClient(config).RequestToken(
-              new AuthorizationCodeTokenRequest(
-                "ClientId", "ClientSecret", response.Code, new Uri("http://localhost:5543/callback")
-              )
-            );
-
-            var spotify = new SpotifyClient(tokenResponse.AccessToken);
-            // do calls with Spotify and save token?
-        }
-
-        private static async Task OnErrorReceived(object sender, string error, string state)
-        {
-            Console.WriteLine($"Aborting authorization, error received: {error}");
-            await _server.Stop();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
