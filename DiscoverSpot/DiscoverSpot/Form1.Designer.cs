184818c7d02cabc6e364efb3e06cb9203c5bcfad
Carter Brown <carter.brown@trojans.dsu.edu>
2023-12-08 22:08:28 -0600

    Fixed error

--- DiscoverSpot/DiscoverSpot/Form1.Designer.cs
+++ DiscoverSpot/DiscoverSpot/Form1.Designer.cs
@@ -57,7 +57,6 @@
             this.label1.Size = new System.Drawing.Size(280, 51);
             this.label1.TabIndex = 1;
             this.label1.Text = "SpotDiscover";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
